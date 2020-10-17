765b0ae3ca7fdfc9be75861d72a4028c123c01e3
livia1031 <liviahernani01234@gmail.com>
2020-10-17 20:38:09 +0700

    Prak2

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,15 @@ class Home extends BaseController
 	{
 		return view('welcome_message');
 	}
+	public function show()
+	{
+		$data['title'] = "Mahasiswa";
+		$data['nama'] = "Livia Ayu Istoria Hernani";
+		$data['npm'] = "1817051031";
+		echo view('mahasiswa/header',$data);
+		echo view('mahasiswa/index',$data);
+		echo view('mahasiswa/footer');
+	}
 
 	//--------------------------------------------------------------------
 
